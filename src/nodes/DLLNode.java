044a50865159939b25eab118fedcd944b85447f2
epeltz33 <epeltz@optonline.net>
2024-11-11 23:46:53 -0500

    pp3

--- src/nodes/DLLNode.java
+++ src/nodes/DLLNode.java
@@ -1,38 +1,72 @@
 package nodes;
 
-public class DLLNode<E> {
+public class DLLNode<E> implements Comparable<DLLNode<E>> {
+	private E data;
+	private DLLNode<E> next;
+	private DLLNode<E> prev;
 
-	  private E data;
-	  private DLLNode<E> next;
-	  private DLLNode<E> prev;
-	  
-	  public DLLNode(E data) {
-	    this.data = data;
-	    next = null;
-	    prev = null;
-	  }
-	 
-	  public void setData(E data) {
-	    this.data = data;
-	  }
+	public DLLNode(E data) {
+		this.data = data;
+		this.next = null;
+		this.prev = null;
+	}
 
-	  public E getData() {
-	    return data;
-	  }
-	 
-	  public void setNext(DLLNode<E> reference) {
-	    this.next = reference;
-	  }
+	public E getData() {
+		return data;
+	}
 
-	  public DLLNode<E> getNext() {
-	    return next;
-	  }
+	public void setData(E data) {
+		this.data = data;
+	}
 
-	  public void setPrev(DLLNode<E> reference) {
-	    this.prev = reference;
-	  }
+	public DLLNode<E> getNext() {
+		return next;
+	}
 
-	  public DLLNode<E> getPrev() {
-	    return prev;
-	  }
+	public void setNext(DLLNode<E> next) {
+		this.next = next;
+	}
+
+	public DLLNode<E> getPrev() {
+		return prev;
+	}
+
+	public void setPrev(DLLNode<E> prev) {
+		this.prev = prev;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+
+		DLLNode<?> other = (DLLNode<?>) obj;
+		if (data == null) {
+			return other.data == null;
+		}
+		return data.equals(other.data);
+	}
+
+	@Override
+	public int compareTo(DLLNode<E> other) {
+		if (other == null) {
+			return 1;
+		}
+		if (this.data == null) {
+			return -1;
+		}
+		if (other.data == null) {
+			return 1;
+		}
+		return ((Comparable<E>) this.data).compareTo(other.data);
+	}
+
+	@Override
+	public String toString() {
+		return data == null ? "null" : data.toString();
+	}
 }
